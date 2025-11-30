faa869403d8ddd2dfe47b81c7761981f1d73c30b
floowxy <alejsot1234@gmail.com>
2025-11-29 20:42:23 -0500

    Update

--- RPN-CALCULATOR/include/stack.h
+++ RPN-CALCULATOR/include/stack.h
@@ -1,9 +1,12 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include <stdio.h>
+
 void push(double value);
 double pop();
 void print_stack();
+void print_stack_file(FILE *f);
 int get_stack_size();
 double get_top_value();
 
