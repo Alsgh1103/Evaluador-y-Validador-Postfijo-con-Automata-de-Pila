598f31739e61dacf9391b3f6bdfc699b65912f60
floowxy <alejsot1234@gmail.com>
2025-11-29 20:42:37 -0500

    Update

--- RPN-CALCULATOR/include/utils.h
+++ RPN-CALCULATOR/include/utils.h
@@ -1,7 +1,10 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <stdio.h>
+
 void print_formatted(double value);
+void print_formatted_file(FILE *f, double value);
 int is_operator(char* token);
 
 #endif
