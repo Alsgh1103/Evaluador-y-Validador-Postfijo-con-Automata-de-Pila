a45b0c3e3d3b2c4438489fc9b04e31c2d4724cba
floowxy <alejsot1234@gmail.com>
2025-11-30 22:15:43 -0500

    Taba de transiciones

--- RPN-CALCULATOR/src/main.c
+++ RPN-CALCULATOR/src/main.c
@@ -8,6 +8,9 @@
 void modo_archivo(char *nombre_entrada) {
     char nombre_res[300];
     char nombre_evo[300];
+    char transicion[300];
+    char descripcion[300];
+    int paso = 0;
 
     sprintf(nombre_res, "resultado_%s", nombre_entrada);
     sprintf(nombre_evo, "evolucion_%s", nombre_entrada);
@@ -21,9 +24,10 @@ void modo_archivo(char *nombre_entrada) {
         exit(1);
     }
 
-    fprintf(f_evo, "ENTRADA    =>   ESTADO DE LA PILA\n");
-    fprintf(f_evo, "---------------------------------\n");
-    fprintf(f_evo, "(Inicio)   =>   ");
+    fprintf(f_evo, "%-4s | %-35s | %-25s | %s\n", "PASO", "TRANSICION", "DESCRIPCION", "PILA");
+    fprintf(f_evo, "-----|-------------------------------------|---------------------------|------------------\n");
+    
+    fprintf(f_evo, "%-4d | %-35s | %-25s | Z0 ", paso, "d(q0, e, Z0) = (q0, Z0)", "INICIO");
     print_stack_file(f_evo);
     fprintf(f_evo, "\n");
 
@@ -31,31 +35,47 @@ void modo_archivo(char *nombre_entrada) {
 
     while (fscanf(f_entrada, "%49s", token) == 1) {
         if (strcmp(token, "=") == 0) break;
-
+        
+        paso++;
+        
         if (is_operator(token)) {
             if (get_stack_size() < 2) {
+                fprintf(f_evo, "%-4d | %-35s | %-25s | ERROR: Stack Underflow\n", paso, "ERROR", "Faltan operandos");
                 fprintf(f_res, "ERROR SINTACTICO: Faltan operandos para operar '%s'.", token);
-                fprintf(f_evo, "%-10s =>   [RECHAZADO: ERROR SINTACTICO - Faltan operandos]\n", token);
+                fclose(f_entrada); fclose(f_res); fclose(f_evo);
                 exit(1);
             }
             double op2 = pop();
             double op1 = pop();
             double res = 0;
 
-            if (strcmp(token, "+") == 0) res = op1 + op2;
-            else if (strcmp(token, "-") == 0) res = op1 - op2;
-            else if (strcmp(token, "*") == 0) res = op1 * op2;
+            if (strcmp(token, "+") == 0) {
+                res = op1 + op2;
+                sprintf(transicion, "d(q0, +, XX) = (q0, X)");
+            }
+            else if (strcmp(token, "-") == 0) {
+                res = op1 - op2;
+                sprintf(transicion, "d(q0, -, XX) = (q0, X)");
+            }
+            else if (strcmp(token, "*") == 0) {
+                res = op1 * op2;
+                sprintf(transicion, "d(q0, *, XX) = (q0, X)");
+            }
             else if (strcmp(token, "/") == 0) {
                 if (op2 == 0) {
+                    fprintf(f_evo, "%-4d | %-35s | %-25s | ERROR: Division por 0\n", paso, "ERROR", "Division por cero.");
                     fprintf(f_res, "ERROR ARITMETICO: Division por cero.");
-                    fprintf(f_evo, "%-10s =>   [RECHAZADO: ERROR ARITMETICO - Division por 0]\n", token);
+                    fclose(f_entrada); fclose(f_res); fclose(f_evo);
                     exit(1);
                 }
                 res = op1 / op2;
+                sprintf(transicion, "d(q0, /, XX) = (q0, X)");
             }
+
+            sprintf(descripcion, "2 POP Y 1 PUSH");
             push(res);
             
-            fprintf(f_evo, "%-10s =>   ", token);
+            fprintf(f_evo, "%-4d | %-35s | %-25s | Z0 ", paso, transicion, descripcion);
             print_stack_file(f_evo);
             fprintf(f_evo, "\n");
         } 
@@ -63,32 +83,45 @@ void modo_archivo(char *nombre_entrada) {
             char *endptr;
             double value = strtod(token, &endptr);
             if (*endptr != '\0') {
+                fprintf(f_evo, "%-4d | %-35s | %-25s | ERROR: Token invalido\n", paso, "ERROR", "Simbolo no reconocido");
                 fprintf(f_res, "ERROR SINTACTICO: Simbolo invalido '%s'.", token);
-                fprintf(f_evo, "%-10s =>   [RECHAZADO: ERROR SINTACTICO - Simbolo invalido]\n", token);
+                fclose(f_entrada); fclose(f_res); fclose(f_evo);
                 exit(1);
             }
+            
+            if (get_stack_size() == 0) {
+                sprintf(transicion, "d(q0, %s, Z0) = (q0, X Z0)", token);
+            } else {
+                sprintf(transicion, "d(q0, %s, X) = (q0, XX)", token);
+            }
+
+            sprintf(descripcion, "1 PUSH");
             push(value);
             
-            fprintf(f_evo, "%-10s =>   ", token);
+            fprintf(f_evo, "%-4d | %-35s | %-25s | Z0 ", paso, transicion, descripcion);
             print_stack_file(f_evo);
             fprintf(f_evo, "\n");
         }
     }
 
+    paso++;
     int size = get_stack_size();
     if (size == 1) {
         printf("Exito. Archivos generados:\n -> %s\n -> %s\n", nombre_res, nombre_evo);
         fprintf(f_res, "Resultado: ");
         print_formatted_file(f_res, get_top_value());
-        fprintf(f_res, "\n");
-        fprintf(f_evo, "(Fin)      =>   [ACEPTADO]\n");
+        
+        fprintf(f_evo, "%-4d | %-35s | %-25s | Z0 ", paso, "d(q0, e, X Z0) = (q0, e)", "ACEPTACION");
+        print_stack_file(f_evo);
+        fprintf(f_evo, "\n");
+
     } else if (size == 0) {
         fprintf(f_res, "ERROR SINTACTICO: Pila vacia.");
-        fprintf(f_evo, "(Fin)      =>   [RECHAZADO: ERROR SINTACTICO - Pila vacia]\n");
+        fprintf(f_evo, "%-4d | %-35s | %-25s | RECHAZO: Pila vacia\n", paso, "RECHAZO", "Falta Z0");
         exit(1);
     } else {
         fprintf(f_res, "ERROR SINTACTICO: Expresion incompleta (Sobran %d numeros).", size);
-        fprintf(f_evo, "(Fin)      =>   [RECHAZADO: ERROR SINTACTICO - Sobran numeros]\n");
+        fprintf(f_evo, "%-4d | %-35s | %-25s | RECHAZO: Sobran datos\n", paso, "RECHAZO", "Pila sucia");
         exit(1);
     }
 
@@ -100,6 +133,10 @@ void modo_archivo(char *nombre_entrada) {
 void modo_consola() {
     srand(time(NULL)); 
     int id = rand() % 10000; 
+    
+    char transicion[300];
+    char descripcion[300];
+    int paso = 0;
 
     char nombre_res[100];
     char nombre_evo[100];
@@ -113,9 +150,10 @@ void modo_consola() {
     printf("Ingrese operacion (ej: 5 3 +) y '=' para terminar.\n");
     printf("--------------------------\n");
 
-    fprintf(f_evo, "ENTRADA    =>   ESTADO DE LA PILA\n");
-    fprintf(f_evo, "---------------------------------\n");
-    fprintf(f_evo, "(Inicio)   =>   ");
+    fprintf(f_evo, "%-4s | %-35s | %-25s | %s\n", "PASO", "TRANSICION", "DESCRIPCION", "PILA");
+    fprintf(f_evo, "-----|-------------------------------------|---------------------------|------------------\n");
+    
+    fprintf(f_evo, "%-4d | %-35s | %-25s | Z0 ", paso, "d(q0, e, Z0) = (q0, Z0)", "INICIO");
     print_stack_file(f_evo);
     fprintf(f_evo, "\n");
 
@@ -125,31 +163,47 @@ void modo_consola() {
         printf("> ");
         if (scanf("%49s", token) == EOF) break;
         if (strcmp(token, "=") == 0) break;
-
+        
+        paso++;
+        
         if (is_operator(token)) {
             if (get_stack_size() < 2) {
+                fprintf(f_evo, "%-4d | %-35s | %-25s | ERROR: Stack Underflow\n", paso, "ERROR", "Faltan operandos");
                 fprintf(f_res, "ERROR SINTACTICO: Faltan operandos para operar '%s'.", token);
-                fprintf(f_evo, "%-10s =>   [RECHAZADO: ERROR SINTACTICO - Faltan operandos]\n", token);
+                fclose(f_res); fclose(f_evo);
                 exit(1);
             }
             double op2 = pop();
             double op1 = pop();
             double res = 0;
 
-            if (strcmp(token, "+") == 0) res = op1 + op2;
-            else if (strcmp(token, "-") == 0) res = op1 - op2;
-            else if (strcmp(token, "*") == 0) res = op1 * op2;
+            if (strcmp(token, "+") == 0) {
+                res = op1 + op2;
+                sprintf(transicion, "d(q0, +, XX) = (q0, X)");
+            }
+            else if (strcmp(token, "-") == 0) {
+                res = op1 - op2;
+                sprintf(transicion, "d(q0, -, XX) = (q0, X)");
+            }
+            else if (strcmp(token, "*") == 0) {
+                res = op1 * op2;
+                sprintf(transicion, "d(q0, *, XX) = (q0, X)");
+            }
             else if (strcmp(token, "/") == 0) {
                 if (op2 == 0) {
+                    fprintf(f_evo, "%-4d | %-35s | %-25s | ERROR: Division por 0\n", paso, "ERROR", "Division por cero.");
                     fprintf(f_res, "ERROR ARITMETICO: Division por cero.");
-                    fprintf(f_evo, "%-10s =>   [RECHAZADO: ERROR ARITMETICO - Division por 0]\n", token);
+                    fclose(f_res); fclose(f_evo);
                     exit(1);
                 }
                 res = op1 / op2;
+                sprintf(transicion, "d(q0, /, XX) = (q0, X)");
             }
+
+            sprintf(descripcion, "2 POP Y 1 PUSH");
             push(res);
             
-            fprintf(f_evo, "%-10s =>   ", token);
+            fprintf(f_evo, "%-4d | %-35s | %-25s | Z0 ", paso, transicion, descripcion);
             print_stack_file(f_evo);
             fprintf(f_evo, "\n");
         } 
@@ -157,50 +211,58 @@ void modo_consola() {
             char *endptr;
             double value = strtod(token, &endptr);
             if (*endptr != '\0') {
+                fprintf(f_evo, "%-4d | %-35s | %-25s | ERROR: Token invalido\n", paso, "ERROR", "Simbolo no reconocido");
                 fprintf(f_res, "ERROR SINTACTICO: Simbolo invalido '%s'.", token);
-                fprintf(f_evo, "%-10s =>   [RECHAZADO: ERROR SINTACTICO - Simbolo invalido]\n", token);
+                fclose(f_res); fclose(f_evo);
                 exit(1);
             }
+            
+            if (get_stack_size() == 0) {
+                sprintf(transicion, "d(q0, %s, Z0) = (q0, X Z0)", token);
+            } else {
+                sprintf(transicion, "d(q0, %s, X) = (q0, XX)", token);
+            }
+
+            sprintf(descripcion, "1 PUSH");
             push(value);
 
-            fprintf(f_evo, "%-10s =>   ", token);
+            fprintf(f_evo, "%-4d | %-35s | %-25s | Z0 ", paso, transicion, descripcion);
             print_stack_file(f_evo);
             fprintf(f_evo, "\n");
         }
     }
 
+    paso++;
     int size = get_stack_size();
     if (size == 1) {
         printf("\nExito. Archivos generados:\n -> %s\n -> %s\n", nombre_res, nombre_evo);
 
         fprintf(f_res, "Resultado: ");
         print_formatted_file(f_res, get_top_value());
-        fprintf(f_res, "\n");
-        fprintf(f_evo, "(Fin)      =>   [ACEPTADO]\n");
+        
+        fprintf(f_evo, "%-4d | %-35s | %-25s | Z0 ", paso, "d(q0, e, X Z0) = (q0, e)", "ACEPTACION");
+        print_stack_file(f_evo);
+        fprintf(f_evo, "\n");
+
     } else if (size == 0) {
         fprintf(f_res, "ERROR SINTACTICO: Pila vacia.");
-        fprintf(f_evo, "(Fin)      =>   [RECHAZADO: ERROR SINTACTICO - Pila vacia]\n");
+        fprintf(f_evo, "%-4d | %-35s | %-25s | RECHAZO: Pila vacia\n", paso, "RECHAZO", "Falta Z0");
         exit(1);
     } else {
         fprintf(f_res, "ERROR SINTACTICO: Expresion incompleta (Sobran %d numeros).", size);
-        fprintf(f_evo, "(Fin)      =>   [RECHAZADO: ERROR SINTACTICO - Sobran numeros]\n");
+        fprintf(f_evo, "%-4d | %-35s | %-25s | RECHAZO: Sobran numeros\n", paso, "RECHAZO", "Pila sucia");
         exit(1);
     }
-
+    
     fclose(f_res);
     fclose(f_evo);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc == 2) {
+    if (argc > 1) {
         modo_archivo(argv[1]);
-    } 
-    else if (argc == 1) {
+    } else {
         modo_consola();
-    } 
-    else {
-        printf("Error: Uso incorrecto.\n");
-        return 1;
     }
     return 0;
 }
